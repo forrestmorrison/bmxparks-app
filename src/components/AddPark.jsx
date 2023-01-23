93288db1b9205cba22489e9c80017e01992de247
forrestmorrison <forrestmorrison@gmail.com>
2023-01-23 16:50:47 -0600

    added submit functionality to form

--- src/components/AddPark.jsx
+++ src/components/AddPark.jsx
@@ -1,32 +1,64 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
 const AddPark = () => {
 
+  const [name, setName] = useState('')
+  const [address, setAddress] = useState('')
+  const [nameError, setNameError] = useState(false)
+  const [addressError, setAddressError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    setNameError(false)
+    setAddressError(false)
+
+    if (name === '') {
+      setNameError(true)
+    }
+
+    if (address === '') {
+      setAddressError(true)
+    }
+
+    if (name && address) {
+      console.log(name, address)
+    }
+  }
+
   return (
     <div className='form-container'>
-      <form noValidate autoComplete="off" className='form'>
+      <form noValidate autoComplete="off" className='form' onSubmit={handleSubmit}>
         <TextField
+          onChange={(e) => setName(e.target.value)}
           fullWidth
           id="outlined-basic" 
           label="park name"
           required
           sx={{
-            margin: 1
+            margin: 1,
+            display: 'block'
           }}
           variant="outlined"
+          error={nameError}
         />
         <TextField
+          onChange={(e) => setAddress(e.target.value)}
           fullWidth
           id="outlined-basic" 
           label="park address"
           required
           sx={{
-            margin: 1
+            margin: 1,
+            display: 'block'
           }}
           variant="outlined"
+          error={addressError}
         />
-        <Button 
+        <Button
+          type="submit"
           color="primary"
           sx={{
             margin: 1
