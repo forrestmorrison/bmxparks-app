937e52a3c4c39fc656eb89940af7b89bae2fe712
forrestmorrison <forrestmorrison@gmail.com>
2023-02-10 22:12:26 -0600

    added turnary operator to HamMenu

--- src/components/HamMenu.jsx
+++ src/components/HamMenu.jsx
@@ -1,8 +1,12 @@
 import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 const HamMenu = () => {
+
+    const user = useSelector(state => state.auth.user)
+
     const [openDrawer, setOpenDrawer] = useState(false)
 
     return (
@@ -11,18 +15,33 @@ const HamMenu = () => {
                 open={openDrawer}
                 onClose={() => setOpenDrawer(false)}
             >
-                <List>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <ListItemText>add park</ListItemText>
-                        </ListItemIcon>
-                    </ListItemButton>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <ListItemText>log out</ListItemText>
-                        </ListItemIcon>
-                    </ListItemButton>
-                </List>
+                { user ? (
+                    <List>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ListItemText>add park</ListItemText>
+                            </ListItemIcon>
+                        </ListItemButton>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ListItemText>log out</ListItemText>
+                            </ListItemIcon>
+                        </ListItemButton>
+                    </List>
+                ) : (
+                    <List>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ListItemText>log in</ListItemText>
+                            </ListItemIcon>
+                        </ListItemButton>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ListItemText>sign up</ListItemText>
+                            </ListItemIcon>
+                        </ListItemButton>
+                    </List>
+                ) }
             </Drawer>
             <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
                 <MenuIcon />
