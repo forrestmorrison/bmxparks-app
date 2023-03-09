ce035a07ac95b210945acdf20064e160f39c573a
forrestmorrison <forrestmorrison@gmail.com>
2023-03-08 20:49:32 -0600

    created useAuthStatus

--- src/pages/ParkPage.jsx
+++ src/pages/ParkPage.jsx
@@ -1,11 +1,21 @@
-import ParkItem from "../components/ParkItem"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getParks } from "../features/parks/parkSlice"
 
-const ParkPage = ({item}) => {
-  return (
-    <div>
-        <ParkItem key={item.id} item={item} />
-    </div>
-  )
+const ParkPage = () => {
+    const dispatch = useDispatch()
+
+    const parks = useSelector(state => state.parks.parks)
+
+    useEffect(() => {
+        dispatch(getParks())
+    }, [])
+
+    return (
+        <div>
+            current park
+        </div>
+    )
 }
 
 export default ParkPage
