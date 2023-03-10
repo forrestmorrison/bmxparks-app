8dd2e00205affe2be46aa1e940b84be0b2ea1509
forrestmorrison <forrestmorrison@gmail.com>
2023-03-10 16:49:02 -0600

    installed react-icons, created RatingReview component

---
+++ src/components/ReviewRating.jsx
@@ -0,0 +1,18 @@
+import {} from "react-icons"
+
+const colors = {
+    orange: "#FFBA5A",
+    grey: "#a9a9a9"
+}
+
+const ReviewRating = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+
+
+export default ReviewRating
