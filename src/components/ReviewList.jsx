5575353198da44f5bd286f7d2d9ebbdd708b9ff4
forrestmorrison <forrestmorrison@gmail.com>
2023-03-14 14:49:41 -0500

    added code to ReviewList

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getReviews } from "../features/reviews/reviewSlice"
+import { Grid } from "@mui/material"
+import ReviewItem from "./ReviewItem"
 
 const ReviewList = () => {
-  return (
-    <div>ReviewList</div>
-  )
+    const dispatch = useDispatch()
+
+    const reviews = useSelector(state => state.reviews.reviews)
+
+    useEffect(() => {
+        dispatch(getReviews())
+    }, [])
+
+    if(reviews.length === 0) {
+        return <p>Be the first to review this park!</p>
+    }
+
+    return (
+        <>
+            <Grid container
+                alignItems="center"
+                justifyContent="center"
+            >
+                {reviews.map((item) => (
+                    <ReviewItem key={item.id} item={item} />
+                ))}
+            </Grid>
+        </>
+    )
 }
 
 export default ReviewList
