38197c26148f48bd1cc8f14088b4b0edd8461d89
forrestmorrison <forrestmorrison@gmail.com>
2023-04-05 19:50:44 -0500

    modified Weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Weather = () => {
   return (
-    <div>Weather</div>
+    <div>
+      <h4>current weather: </h4>
+    </div>
   )
 }
 
