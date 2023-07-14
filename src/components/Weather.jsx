8e269eeb48dc7c1705943be6adf434cae924178c
forrestmorrison <forrestmorrison@gmail.com>
2023-07-13 21:01:25 -0500

    moved Auth0 button components to auth folder

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,18 +3,8 @@ import { useState } from 'react'
 
 const Weather = () => {
   const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=austin&appid={apiKey}`
-
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
-      })
-    }
-  }
 
   return (
     <div className="weather-component">
