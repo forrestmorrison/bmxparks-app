592c13a48339bed3075f8f549fec4c083d2185f4
forrestmorrison <forrestmorrison@gmail.com>
2023-07-14 21:29:45 -0500

    added MUI button to LoginButton & LogoutButton

--- src/auth/LoginButton.jsx
+++ src/auth/LoginButton.jsx
@@ -1,12 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mui/material"
 
 const LoginButton = () => {
     const { loginWithRedirect, isAuthenticated } = useAuth0()
     return (
         !isAuthenticated && (
-            <button onClick={() => loginWithRedirect()}>
+            <Button 
+                onClick={() => loginWithRedirect()}
+                sx={{
+                    m: 1,
+                    "&:hover": {
+                        backgroundColor: "purple",
+                        color: "white",
+                    }
+                }}
+            >
                 Sign In
-            </button>
+            </Button>
         )
     )
 }
--- src/auth/LogoutButton.jsx
+++ src/auth/LogoutButton.jsx
@@ -1,12 +1,22 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mui/material";
 
 const LogoutButton = () => {
     const { logout, isAuthenticated } = useAuth0()
     return (
         isAuthenticated && (
-            <button onClick={() => logout()}>
+            <Button 
+                onClick={() => logout()}
+                sx={{
+                    m: 1,
+                    "&:hover": {
+                        backgroundColor: "purple",
+                        color: "white",
+                    }
+                }}
+            >
                 Sign Out
-            </button>
+            </Button>
         )
     )
 }
